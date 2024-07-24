73fcd7d41538d2e581c7bd8d95c650f1d0cc118e
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-24 14:08:48 -0700

    Task 2: Arrow functions

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
