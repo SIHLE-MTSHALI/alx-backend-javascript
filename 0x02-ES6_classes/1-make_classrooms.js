323baeeb05ea738175da4147028f5e58887e5c0e
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-25 16:51:16 -0700

    Task 11: Implement EVCar class extending Car

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 }
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
