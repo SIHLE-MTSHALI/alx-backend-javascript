36bfe8daf588f71c70b72d2fc6382f192132c677
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-25 17:12:33 -0700

    Task 11: EVCar extends Car with cloneCar behavior

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
-
 import Car from './10-car.js';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+    if (typeof range !== 'string') {
+      throw new TypeError('Range must be a string');
+    }
     this._range = range;
   }
 
   cloneCar() {
-    const clonedCar = super.cloneCar();
-    clonedCar._range = undefined;
-    return clonedCar;
+    const newCar = new Car();
+    for (const prop of Object.getOwnPropertyNames(this)) {
+      if (prop !== '_range') {
+        newCar[prop] = this[prop];
+      }
+    }
+    return newCar;
   }
 }
-
-export default EVCar;
