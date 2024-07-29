6babcf3ad0055f2467d7bc807ab2260b3a3d2d1a
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-29 09:53:50 -0700

    Task 5: Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+import createInt8TypedArray from "./5-typed_arrays.js";
+
+console.log(createInt8TypedArray(10, 2, 89));
---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const createInt8TypedArray = (length, position, value) => {
+  if (position >= length) throw new Error('Position outside range');
+  
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+  
+  return new DataView(buffer);
+};
+
+export default createInt8TypedArray;
