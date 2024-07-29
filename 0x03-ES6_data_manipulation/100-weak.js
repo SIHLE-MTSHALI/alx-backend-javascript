bbfcbf13402a4020f5c58eaa2d7c7c67f60e64e1
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-29 10:31:31 -0700

    Task 11: Weak link data structure (Advanced)

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+export const weakMap = new WeakMap();
+
+export const queryAPI = (endpoint) => {
+  let count = weakMap.get(endpoint) || 0;
+  count++;
+  
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  
+  weakMap.set(endpoint, count);
+};
