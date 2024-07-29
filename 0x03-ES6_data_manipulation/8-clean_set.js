3dfcaf9dd1154b7a2da1724b809032229d66c143
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-29 10:01:52 -0700

    Task 8: Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const cleanSet = (set, startString) => {
+  if (typeof startString !== 'string' || startString === '') return '';
+
+  return Array.from(set)
+    .filter(value => typeof value === 'string' && value.startsWith(startString))
+    .map(value => value.slice(startString.length))
+    .join('-');
+};
+
+export default cleanSet;
