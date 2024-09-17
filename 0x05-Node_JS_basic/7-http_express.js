f172a796a1228a7acb4f2ee1023e6d7e683e150c
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-17 14:16:54 -0700

    Task 7: Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,7 @@
-#!/usr/bin/node
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
-
-function countStudents(res, path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Cannot load the database');
-    } else {
-      let lines = data.split('\n').filter((line) => line.trim() !== '');
-      lines = lines.slice(1);
-      let responseText = `This is the list of our students\n`;
-      responseText += `Number of students: ${lines.length}\n`;
-
-      const fields = {};
-      for (const line of lines) {
-        const [firstname, lastname, age, field] = line.split(',');
-        if (field) {
-          if (!fields[field]) fields[field] = [];
-          fields[field].push(firstname);
-        }
-      }
-      for (const [field, students] of Object.entries(fields)) {
-        responseText += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
-      }
-      res.send(responseText.trim());
-    }
-  });
-}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -36,7 +9,30 @@ app.get('/', (req, res) => {
 
 app.get('/students', (req, res) => {
   const db = process.argv[2];
-  countStudents(res, db);
+  res.write('This is the list of our students\n');
+
+  fs.readFile(db, 'utf8', (err, data) => {
+    if (err) {
+      res.end('Cannot load the database');
+    } else {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+      res.write(`Number of students: ${students.length}\n`);
+
+      const fields = {};
+      for (const line of students) {
+        const [firstname, lastname, age, field] = line.split(',');
+        if (field) {
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        }
+      }
+      for (const [field, group] of Object.entries(fields)) {
+        res.write(`Number of students in ${field}: ${group.length}. List: ${group.join(', ')}\n`);
+      }
+      res.end();
+    }
+  });
 });
 
 app.listen(1245);
