3c3926a227f2c70aa5b982cea81644561a20d710
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-17 13:28:19 -0700

    Task 7: Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,26 +1,44 @@
 #!/usr/bin/node
 const express = require('express');
-const { countStudents } = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
-const port = 1245;
+
+function countStudents(res, path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Cannot load the database');
+    } else {
+      let lines = data.split('\n').filter((line) => line.trim() !== '');
+      lines = lines.slice(1);
+      let responseText = `This is the list of our students\n`;
+      responseText += `Number of students: ${lines.length}\n`;
+
+      const fields = {};
+      for (const line of lines) {
+        const [firstname, lastname, age, field] = line.split(',');
+        if (field) {
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        }
+      }
+      for (const [field, students] of Object.entries(fields)) {
+        responseText += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+      }
+      res.send(responseText.trim());
+    }
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.write('This is the list of our students\n');
-  try {
-    const data = await countStudents(process.argv[2]);
-    res.end(data);
-  } catch (error) {
-    res.end(error.message);
-  }
+app.get('/students', (req, res) => {
+  const db = process.argv[2];
+  countStudents(res, db);
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.listen(1245);
 
 module.exports = app;
