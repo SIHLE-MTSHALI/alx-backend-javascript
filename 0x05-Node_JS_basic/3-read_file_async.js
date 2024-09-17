2b69e2e345d68ddd46a70f55738d5a66184f2f25
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-17 14:15:06 -0700

    Task 3: Reading a file asynchronously with Node.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 const fs = require('fs');
 
 function countStudents(path) {
@@ -7,24 +6,26 @@ function countStudents(path) {
       if (err) {
         reject(Error('Cannot load the database'));
       } else {
-        let lines = data.split('\n').filter((line) => line.trim() !== '');
-        lines = lines.slice(1);
-        console.log(`Number of students: ${lines.length}`);
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        if (lines.length === 0) {
+          reject(Error('Cannot load the database'));
+        } else {
+          const students = lines.slice(1);
+          console.log(`Number of students: ${students.length}`);
 
-        const fields = {};
-        for (const line of lines) {
-          const [firstname, lastname, age, field] = line.split(',');
-          if (field) {
-            if (!fields[field]) fields[field] = [];
-            fields[field].push(firstname);
+          const fields = {};
+          for (const line of students) {
+            const [firstname, lastname, age, field] = line.split(',');
+            if (field) {
+              if (!fields[field]) fields[field] = [];
+              fields[field].push(firstname);
+            }
           }
+          for (const [field, group] of Object.entries(fields)) {
+            console.log(`Number of students in ${field}: ${group.length}. List: ${group.join(', ')}`);
+          }
+          resolve();
         }
-        for (const [field, students] of Object.entries(fields)) {
-          console.log(
-            `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
-          );
-        }
-        resolve();
       }
     });
   });
