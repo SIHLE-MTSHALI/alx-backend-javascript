2012f98dc85e8be254bdac118968b332c5c85b5c
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-19 13:58:19 -0700

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,13 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+
+/**
+ * Calculates the sum of two numbers after rounding them.
+ * @param {number} a - First number.
+ * @param {number} b - Second number.
+ * @returns {number} The sum of rounded a and b.
+ */
 function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
 }
