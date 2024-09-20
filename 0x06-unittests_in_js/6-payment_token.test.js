04a47541bd7e7f7126513455e132865f64199973
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-19 17:05:54 -0700

    Task 6: Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should resolve with correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
