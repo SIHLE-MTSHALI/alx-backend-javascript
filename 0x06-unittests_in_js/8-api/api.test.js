a960c93a32baf613125126351fba175d7e3d5138
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-19 17:15:11 -0700

    Task 8: Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('correct status code', (done) => {
+    request.get(API_URL, (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('correct result', (done) => {
+    request.get(API_URL, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
