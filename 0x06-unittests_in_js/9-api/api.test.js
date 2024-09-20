0cdbbb663a29e880a9e93b28ceb9346b04bc880a
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-19 17:21:35 -0700

    Task 9: Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api.js');
+
+describe('Index page', () => {
+  it('should return 200 status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return 200 status code for numeric id', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message for numeric id', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 status code for non-numeric id', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
